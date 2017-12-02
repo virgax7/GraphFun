1ad52a3da6f6613e6c6c50bf83b748669815999f
Pyoung Kang Kim <pyoungkangkim@gmail.com>
2017-12-02 12:50:48 -0500

    Functionalized Maze some more

--- src/main/java/MyGraphs/chapter4dot1/Exercises/maze/Maze.java
+++ src/main/java/MyGraphs/chapter4dot1/Exercises/maze/Maze.java
@@ -15,7 +15,7 @@ class Maze {
     Maze(final int n) {
         locations = new Location[n][n];
         this.n = n;
-        initializeLocations();
+        IntStream.range(0, this.n).forEach(i -> Arrays.setAll(locations[i], j -> new Location(new XyCoordinate(i, j))));
         dfs(new XyCoordinate(0, 0), new XyCoordinate(-1, -1));
     }
 
@@ -28,19 +28,9 @@ class Maze {
         for (final DirectionProber directionProber : directions) {
             if (directionProber.directionIsValid(locations, curLocation)) {
                 dfs(directionProber.getNextCoordinate(curLocation), xyCoordinate);
-            } else {
-                if (directionProber.getNextCoordinate(curLocation).getX() != prevXyCoordinate.getX()
+            } else if (directionProber.getNextCoordinate(curLocation).getX() != prevXyCoordinate.getX()
                         || directionProber.getNextCoordinate(curLocation).getY() != prevXyCoordinate.getY()) {
                     directionProber.closeDirection(curLocation);
-                }
-            }
-        }
-    }
-
-    private void initializeLocations() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                locations[i][j] = new Location(new XyCoordinate(i, j));
             }
         }
     }
@@ -50,23 +40,17 @@ class Maze {
         final StringBuilder maze = new StringBuilder();
         IntStream.range(0, n).forEach($ -> maze.append("....."));
         maze.append("\n");
-
-        for (int i = 0; i < n; i++) {
-            final StringBuilder leftWall = new StringBuilder();
-            final StringBuilder downWall = new StringBuilder();
-            fillLeftAndDownWalls(locations[i], leftWall, downWall);
-            maze.append(leftWall.toString() + "|\n");
-            maze.append(downWall.toString() + "\n");
-        }
+        IntStream.range(0, n).forEach(i -> fillLeftAndDownWalls(locations[i], new StringBuilder(), new StringBuilder(), maze));
         return maze.toString();
     }
 
-    private void fillLeftAndDownWalls(final Location[] location, final StringBuilder leftWall, final StringBuilder downWall) {
-        for (int j = 0; j < n; j++) {
-            final Location curLocation = location[j];
+    private void fillLeftAndDownWalls(final Location[] location, final StringBuilder leftWall, final StringBuilder downWall, final StringBuilder maze) {
+        IntStream.range(0, n).boxed().map(i -> location[i]).forEach(curLocation -> {
             leftWall.append(curLocation.isLeftClosed() ? "|    " : "     ");
             downWall.append(curLocation.isDownClosed() ? "....." : "     ");
-        }
+        });
+        maze.append(leftWall.toString() + "|\n");
+        maze.append(downWall.toString() + "\n");
     }
 
 }
